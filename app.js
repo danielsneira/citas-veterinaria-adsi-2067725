2b66d29ccfacfa7564878ab66d60f0ecd80ccff7
Daniel Neira <danos.neira@gmail.com>
2021-06-03 14:13:51 -0500

    deploy

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import path from 'path';
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
+import propietario from "./routes/propietario";
+import cita from "./routes/cita";
 
 const app = express();
-
 // Middleware
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+//routes
+app.use("/api/citas", cita);
+app.use("/api/propietarios", propietario);
+
 
 // Middleware para Vue.js router modo history
-const history = require('connect-history-api-fallback');
+const history = require("connect-history-api-fallback");
 app.use(history());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set('puerto', process.env.PORT || 3000);
-app.listen(app.get('puerto'), () => {
-  console.log('Example app listening on port'+ app.get('puerto'));
+app.set("puerto", process.env.PORT || 3000);
+app.listen(app.get("puerto"), () => {
+	console.log("Example app listening on port " + app.get("puerto"));
 });
+
+// Conexión base de datos
+
+// const uri = "mongodb://localhost:27017/myapp";
+const uri = process.env.MONGODB_URI;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+// Or using promises
+mongoose.connect(uri, options).then(
+	() => {
+		console.log("Conectado a DB");
+	},
+	(err) => {
+		console.log(err);
+	}
+);
+
